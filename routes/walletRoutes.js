7328ca63759f4d928db3c9920f3b6e2eaf493b25
Md.Nobin <mdnobin.fdr@gmail.com>
2025-10-06 20:16:57 +0600

    new user api make update user controlar and user router

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { addMoney, cashout, current } from "../controllers/walletController.js";
-import verifyFirebaseToken from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/addmoney/", verifyFirebaseToken, addMoney);
-router.post("/cashout/", verifyFirebaseToken, cashout);
-router.get("/current/", verifyFirebaseToken, current);
+router.post("/addmoney/",  addMoney);
+router.post("/cashout/",  cashout);
+router.get("/current/",  current);
 
 export default router;
