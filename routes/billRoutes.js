c2ccb81de8e28a42b18172bd04ae6bc1ef741db5
Md.Nobin <mdnobin.fdr@gmail.com>
2025-10-19 20:59:31 +0600

    bills controller update and bills model and update bill routes

---
+++ routes/billRoutes.js
@@ -0,0 +1,16 @@
+// routes/splitBillRoutes.js
+import express from "express";
+import {
+  createBill,
+  getBills,
+} from "../controllers/billController.js";
+
+const router = express.Router();
+
+// create new bill
+router.post("/", createBill);
+
+// get bills (optionally filter by user)
+router.get("/", getBills);
+
+export default router;
